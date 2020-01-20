9f9cd7e3d9ee84717a602c8c13d4be0f17418ecf
Alex Roden <b.r_alex@hotmail.co.uk>
2020-01-20 21:14:09 +0000

    Add more string helpers

--- helpers/Str.js
+++ helpers/Str.js
@@ -1,4 +1,21 @@
 exports.Str = {
+    camel: (string) => {
+        let str = this.Str.title(string);
+
+        return `${str.charAt(0).toLowerCase()}${str.slice(1)}`;
+    },
+    slug: (string, separator = "-") => {
+        return string.toLowerCase().replace(" ", separator);
+    },
+    title: (string) => {
+        let splitStr = string.toLowerCase().split(" ");
+
+        for (let i = 0; i < splitStr.length; i++) {
+            splitStr[i] = this.Str.ucUpper(splitStr[i]);
+        }
+
+        return splitStr.join(" ")
+    },
     ucUpper: (string) => {
         return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
     }
