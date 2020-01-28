2cdb1874698e133f17674ae2486cb8dd86f76133
Alex Roden <b.r_alex@hotmail.co.uk>
2020-01-28 20:26:39 +0000

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: "./resources/assets/js/app.js",
+    output: {
+        path: path.resolve(__dirname, "public/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    // options: {
+                    //     presets: ["es2015"]
+                    // }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.css"
+        }),
+        new CleanWebpackPlugin()
+    ],
+};
