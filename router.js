e1750e65358b0e12040f32cd4986df122476fa6f
Alex Roden <b.r_alex@hotmail.co.uk>
2020-02-10 21:08:53 +0000

    Implement bundles

--- router.js
+++ router.js
@@ -1,10 +1,35 @@
+const fs = require("fs");
 const url = require("url");
 
-// Routes
 const web = require("./app/http/routes/web");
 const api = require("./app/http/routes/api");
 
+/**
+ * @type string
+ */
 const API_ROUTE = "api";
+
+/**
+ * @type string
+ */
+const DIST_ROUTE = "dist";
+
+/**
+ * @type string
+ */
+const PUBLIC_ROUTE = "public";
+
+/**
+ * @type Object
+ */
+const EXTENSIONS = {
+    "html": "text/html",
+    "css": "text/css",
+    "js": "application/javascript",
+    "png": "image/png",
+    "gif": "image/gif",
+    "jpg": "image/jpeg"
+};
 
 exports.router = (req, res) => {
     const baseURI = url
@@ -14,21 +39,54 @@ exports.router = (req, res) => {
     const segments = pathname.substr(1).split("/");
 
     let routes = null;
-    if (segments[0] === API_ROUTE) {
-        routes = api;
-        pathname = pathname.replace(`/${API_ROUTE}`, "");
-        if (pathname === "") {
-            pathname = "/";
-        }
-    } else {
-        routes = web;
-    }
+    let route = null;
+    switch (segments[0]) {
+        case API_ROUTE:
+            routes = api;
+            pathname = pathname.replace(`/${API_ROUTE}`, "");
+            if (pathname === "") {
+                pathname = "/";
+            }
 
-    const route = routes.routes[req.method][pathname];
-    if (route === undefined) {
-        res.writeHead(404);
-        res.end("Content not found!");
-    } else {
-        route(req, res);
+            route = routes.routes[req.method][pathname];
+            if (route === undefined) {
+                res.writeHead(404);
+                res.end("Content not found!");
+            } else {
+                route(req, res);
+            }
+
+            break;
+        case PUBLIC_ROUTE:
+            if (segments[1] === DIST_ROUTE) {
+                const file = segments[2];
+                const extension = file.split(".")[1];
+
+                if (fs.existsSync(__dirname + `/public/dist/${file}`)) {
+                    fs.readFile(
+                        __dirname + `/public/dist/${file}`,
+                        (error, data) => {
+                            res.writeHead(200, {'Content-type': EXTENSIONS[extension]});
+                            res.write(data);
+                            res.end();
+                        }
+                    );
+                } else {
+                    res.writeHead(404);
+                    res.end("Content not found!");
+                }
+            }
+
+            break;
+        default:
+            routes = web;
+
+            route = routes.routes[req.method][pathname];
+            if (route === undefined) {
+                res.writeHead(404);
+                res.end("Content not found!");
+            } else {
+                route(req, res);
+            }
     }
 };
